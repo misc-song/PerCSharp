f3d2fd4bc59294e335fb0adfbcbead31c206537f
song hejun <wqshj@126.com>
2017-01-23 01:28:47 +0800

    委托
    简单委托 多播委托

---
+++ 委托/委托/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 委托
+{
+    public delegate void GetString();//创建委托
+    public class Funcs
+    {
+        public static void Fun1()//创建静态方法
+        {
+            Console.WriteLine("这是Fun1");
+
+        }
+        public static void Fun2()
+        {
+            Console.WriteLine("这是Fun2");
+
+        }
+        public static void Fun3()
+        {
+            Console.WriteLine("这是Fun3");
+
+        }
+    }
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            GetString FirstFunc = new GetString(Funcs.Fun1);//实例化委托
+            GetString SecondFunc = new GetString(Funcs.Fun2);
+            GetString ThirdFunc = new GetString(Funcs.Fun3);
+            FirstFunc += SecondFunc;//多播委托
+            FirstFunc += ThirdFunc;
+            FirstFunc.Invoke();//执行委托
+        }
+
+    }
+}
